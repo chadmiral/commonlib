a72ea94cdee9ce6d4d02d5b8d82b90da2e6eb29a
Chad Foxglove <chandra.foxglove@gmail.com>
2016-12-09 20:27:57 -0800

    animation exporter

--- graphics/blender_scripts/io_anim/export_anim.py
+++ graphics/blender_scripts/io_anim/export_anim.py
@@ -16,16 +16,24 @@ def write(filepath,
 	# write the header of the file
 	file = open(filepath, "w")
 	file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n")
-	file.write("<animation version=\"1\">\n")
+	anim_length = 100 #TODO
+	file.write("<animation version=\"1\" length_frames=\"%.6f\">\n" % anim_length)
 
 	if bpy.context.active_object:
 		armature = bpy.context.active_object
 		for curve in armature.animation_data.action.fcurves:
 			bone_name = curve.data_path.split('"')[1]
 
-			file.write("\t<curve name=\"")
+			file.write("\t<curve bone=\"")
 			file.write(bone_name)
 			file.write("\">\n")
+			for frame in range(1, 10):
+				y = curve.evaluate(frame)
+				file.write("\t\t<frame>%.6f %.6f</frame>\n" % (frame, y))
+
+			#for kp = curve.keyframe_points:
+				#kp.co : vec2
+				#file.write("\t\t<keyframe>")
 
 			file.write("\t</curve>\n")
 
